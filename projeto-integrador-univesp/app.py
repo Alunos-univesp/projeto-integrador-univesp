0a0de87fa4b90577a3639a54549f3b6f807e73d0
Alunos-univesp <163595277+Alunos-univesp@users.noreply.github.com>
2024-04-08 12:27:10 -0300

    Update app.py

--- projeto-integrador-univesp/app.py
+++ projeto-integrador-univesp/app.py
@@ -1,65 +1,49 @@
-from flask import Flask, render_template, request, redirect, url_for, g, flash
+import sys
+import logging
+from flask import Flask, render_template, request, redirect, url_for, g, flash, jsonify
 import os
 import sqlite3
 from datetime import datetime, timedelta
 import firebase_admin
 from firebase_admin import credentials, firestore
-import sys
-from flask import jsonify
-from werkzeug.urls import unquote
 
 def get_cred_file_path():
-    # Verifica se estamos rodando no modo "frozen" usando o PyInstaller
     if getattr(sys, 'frozen', False):
-        # Se for o caso, o caminho é o diretório onde o executável está localizado
         application_path = sys._MEIPASS
     else:
-        # Caso contrário, apenas use o diretório atual do script
         application_path = os.path.dirname(os.path.abspath(__file__))
-
     cred_file_name = 'controle-de-mercadoria-firebase-adminsdk-z1zlf-036e096d97.json'
     return os.path.join(application_path, cred_file_name)
 
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
-# Inicialize o Firebase Admin SDK com suas credenciais
-cred = credentials.Certificate("controle-de-mercadoria-firebase-adminsdk-z1zlf-036e096d97.json")
-# Inicialize o Firebase Admin SDK com suas credenciais
+# Restante do código...
+
+
 cred_path = get_cred_file_path()
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
-
-
-# Obtenha uma referência para o banco de dados Firestore
 db = firestore.client()
 
 app = Flask(__name__)
 app.secret_key = 'UNIVESP'
 
-# Define o caminho para o banco de dados SQLite principal
 DATABASE = os.path.join(os.getcwd(), 'meu_banco_de_dados.db')
-
-# Define o caminho para o banco de dados SQLite dos produtos que saíram
 SAIDAS_DATABASE = os.path.join(os.getcwd(), 'produtos_que_sairam.db')
 
-
-
 def get_db(database=None):
-    # Se nenhum banco de dados for especificado, usa o banco de dados principal
     if database is None:
         database = DATABASE
-    # Tenta recuperar uma conexão de banco de dados existente do contexto global 'g'
     db = getattr(g, '_database_' + database, None)
-    # Se não houver conexão existente, cria uma nova
     if db is None:
         db = sqlite3.connect(database)
-        db.row_factory = sqlite3.Row  # Configura a fábrica de linhas para retornar dicionários
-        setattr(g, '_database_' + database, db)  # Salva a conexão no contexto global 'g' para reuso
+        db.row_factory = sqlite3.Row
+        setattr(g, '_database_' + database, db)
     return db
 
 def init_db():
-    with app.app_context():  # Usa o contexto da aplicação para garantir acesso aos recursos do Flask
-        db_main = get_db()  # Obtém a conexão com o banco de dados principal
-        # Cria a tabela de produtos no banco de dados, se ainda não existir
+    with app.app_context():
+        db_main = get_db()
         db_main.execute('''
             CREATE TABLE IF NOT EXISTS produtos (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -73,7 +57,6 @@ def init_db():
                 data_validade TEXT
             );
         ''')
-        # Cria a tabela de saídas no banco de dados, se ainda não existir
         db_main.execute('''
             CREATE TABLE IF NOT EXISTS saidas (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -83,11 +66,9 @@ def init_db():
                 FOREIGN KEY (produto_id) REFERENCES produtos (id)
             );
         ''')
-        db_main.commit()  # Salva as alterações no banco de dados principal
-        
-        # Inicializar banco de dados dos produtos que saíram
-        db_saidas = get_db(SAIDAS_DATABASE)  # Obtém a conexão com o banco de dados de saídas
-        # Cria a tabela de saídas de produtos, se ainda não existir
+        db_main.commit()
+
+        db_saidas = get_db(SAIDAS_DATABASE)
         db_saidas.execute('''
             CREATE TABLE IF NOT EXISTS produtos_saidas (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -99,25 +80,23 @@ def init_db():
                 data_saida TEXT NOT NULL
             )
         ''')
-        db_saidas.commit()  # Salva as mudanças no banco de dados de saídas
+        db_saidas.commit()
 
-# Fecha a conexão com o banco de dados quando o contexto da aplicação termina
 @app.teardown_appcontext
 def close_connection(exception):
-    db_main = getattr(g, '_database', None)  # Tenta obter a conexão com o banco de dados do contexto global 'g'
+    db_main = getattr(g, '_database', None)
     if db_main is not None:
-        db_main.close()  # Fecha a conexão se existir
+        db_main.close()
     db_saidas = getattr(g, '_database_' + SAIDAS_DATABASE, None)
     if db_saidas is not None:
         db_saidas.close()
-        
+
 def sincronizar_produtos_com_sqlite():
-    produtos_ref = db_firestore.collection('produtos')
+    produtos_ref = db.collection('produtos')
     docs = produtos_ref.stream()
     db_sqlite = get_db()
     for doc in docs:
         produto = doc.to_dict()
-        # Verifica se o produto já existe no SQLite para evitar duplicação
         existe = db_sqlite.execute('SELECT id FROM produtos WHERE id = ?', (doc.id,)).fetchone()
         if not existe:
             db_sqlite.execute('''INSERT INTO produtos (id, nome, codigo, custo, marca, informacoes, quantidade, data_inclusao, data_validade) 
@@ -127,16 +106,15 @@ def sincronizar_produtos_com_sqlite():
             db_sqlite.execute('''UPDATE produtos SET nome=?, codigo=?, custo=?, marca=?, informacoes=?, quantidade=?, data_inclusao=?, data_validade=? WHERE id=?''',
                                 (produto.get('nome'), produto.get('codigo'), produto.get('custo'), produto.get('marca'), produto.get('informacoes'), produto.get('quantidade'), produto.get('data_inclusao'), produto.get('data_validade'), doc.id))
         db_sqlite.commit()
-# Função para adicionar produtos que saíram ao banco de dados de saída
+
 def add_produto_saida(produto_id, nome, marca, custo, quantidade):
-    with app.app_context():  # Garante que a função seja executada no contexto da aplicação Flask
-        db = get_db(SAIDAS_DATABASE)  # Obtém a conexão com o banco de dados de saídas
-        # Formata a data atual no formato apropriado para armazenamento
+    with app.app_context():
+        db = get_db(SAIDAS_DATABASE)
         data_saida = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        # Insere o registro da saída do produto na tabela de saídas
         db.execute('INSERT INTO produtos_saidas (produto_id, nome, marca, custo, quantidade, data_saida) VALUES (?, ?, ?, ?, ?, ?)',
                    (produto_id, nome, marca, custo, quantidade, data_saida))
-        db.commit()  # Salva as alterações no banco de dados
+        db.commit()
+
 @app.route('/')
 def index():
     return render_template('index.html')
